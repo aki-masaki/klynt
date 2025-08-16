6d2efb4742ef78f4042ceeae706263bdfbe894d9
aki-masaki <nikistefan95@gmail.com>
2025-08-16 19:14:19 +0300

    feat: added transpile function to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
+use crate::transpiler::Transpiler;
+use crate::parser::Parser;
+use crate::lexer::Lexer;
+
 pub mod lexer;
 pub mod parser;
 pub mod ast;
 pub mod transpiler;
+
+pub fn transpile(input: String) -> String {
+    let lexer = Lexer::new(input);
+    let mut parser = Parser::new(lexer);
+    let transpiler = Transpiler::new(parser.parse(false));
+
+    transpiler.transpile()
+}
